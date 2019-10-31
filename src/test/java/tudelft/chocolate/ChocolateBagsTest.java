d91fc964cc0666c82bb3fb9de3c21b904d2da679
Wilco Siebers <wilco.siebers@topic.nl>
2019-10-31 07:42:01 +0100

    week3

--- src/test/java/tudelft/chocolate/ChocolateBagsTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class ChocolateBagsTest {
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({ "1,1,5,0", "1,1,6,1", "1,1,7,-1", "1,1,8,-1" })
+    @CsvSource({ "1,1,5,0", "1,1,6,1", "1,1,7,-1", "1,1,8,-1", "2,1,8,-1" })
     public void totalIsTooBig(int small, int big, int total, int expectedResult) {
         ChocolateBags bags = new ChocolateBags();
         int result = bags.calculate(small, big, total);
@@ -22,8 +22,8 @@ public class ChocolateBagsTest {
 
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
     @CsvSource({
-        "0,3,17,-1", "1,3,17,-1", "2,3,17,2", "3,3,17,2",
-        "0,3,12,-1", "1,3,12,-1", "2,3,12,2", "3,3,12,2"})
+            "0,3,17,-1", "1,3,17,-1", "2,3,17,2", "3,3,17,2",
+            "0,3,12,-1", "1,3,12,-1", "2,3,12,2", "3,3,12,2"})
     public void bigAndSmallBars(int small, int big, int total, int expectedResult) {
         int result = new ChocolateBags().calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);
--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -9,12 +9,22 @@ public class CountLettersTest {
     public void multipleMatchingWords() {
         int words = new CountLetters().count("cats|dogs");
         Assertions.assertEquals(2, words);
+        words = new CountLetters().count("tiger|bear");
+        Assertions.assertEquals(2, words);
     }
 
     @Test
     public void lastWordDoesNotMatch() {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
+        words = new CountLetters().count("tiger|dog");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void lastCharactersIsNotALetter() {
+        int words = new CountLetters().count("cat|dog|");
+        Assertions.assertEquals(0, words);
     }
 
 }
